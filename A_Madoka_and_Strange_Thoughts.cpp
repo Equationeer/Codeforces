ba3d56aafc4265f5238e14d70bba53914835e16a
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-06-18 19:09:15 +0530

    Madoka_and_Strange_Thoughts

---
+++ A_Madoka_and_Strange_Thoughts.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    int ans = n+(n/2)*2+(n/3)*2;
+    cout<<ans<<"\n";
+}
+
+int main() {
+  int t;
+  cin>>t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}

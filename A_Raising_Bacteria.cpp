b201ffd8fb6cfd0d03a6c47276ef268db8d55fd5
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-06-20 13:13:51 +0530

    Raising_Bacteria

---
+++ A_Raising_Bacteria.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int countSetBits(int n){
+    int count = 0;
+    while(n){
+        n = n&(n-1);
+        count++;
+    }
+    return count;
+}
+ 
+int main() {
+    long long x;
+    cin>>x;
+    cout<<countSetBits(x);
+    return 0;
+}

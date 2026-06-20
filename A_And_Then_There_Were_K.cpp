2915905648a3ac7899a9fa05b2e5a67e18f1ae6f
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-06-20 23:03:56 +0530

    And_Then_There_Were_K

---
+++ A_And_Then_There_Were_K.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+  long long n;
+  cin>>n;
+ int c=0;
+ while(n){
+    n/=2;
+    c++;
+ }
+ int ans = (1<<(c-1))-1;
+  cout<<ans<<"\n";
+}
+ 
+int main() {
+   int t;
+   cin>>t;
+   while(t--){
+    solve();
+   }
+}

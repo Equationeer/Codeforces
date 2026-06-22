2cc7a425c031a98ef9f893eab3118eaced5842eb
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-06-22 19:18:33 +0530

    Symmetric_Encoding

---
+++ B_Symmetric_Encoding.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    set<char>st;
+    for(int i=0;i<n;i++){
+        st.insert(s[i]);
+    }
+    vector<char>v;
+    for(auto &it:st){
+        v.push_back(it);
+    }
+    map<char,char>mp;
+    for(int i=0;i<v.size();i++){
+        mp[v[i]] = v[v.size()-i-1];
+    }
+    for(int i=0;i<n;i++){
+        cout<<mp[s[i]];
+    }
+    cout<<'\n';
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
